881ce3a3742d9c74eb1f30857943a26abc2bb04a
Josh Adebayo <Josh.Adebayo@InfiniteCampus.com>
2023-06-20 11:54:29 -0500

    Bug fixes.

--- src/MacroNutrients.java
+++ src/MacroNutrients.java
@@ -4,6 +4,7 @@ public class MacroNutrients {
     public static void main(String[] args) {
         Scanner info = new Scanner(System.in);
         int tracker = 1;
+        int counter = 0;
 
 
         while (tracker <= 3) {
@@ -11,7 +12,7 @@ public class MacroNutrients {
                 System.out.println("How old are you?");
                 int age = info.nextInt();
                 if (age > 0) {
-                    System.out.println(age);
+//                    System.out.println(age);
                     tracker += 1;
                     System.out.println("What is your gender?");
                 }
@@ -23,16 +24,21 @@ public class MacroNutrients {
 
 
             if (tracker == 2) {
-
                 String gender = info.nextLine();
                 if (gender.equals("male") || gender.equals("female")) {
                     System.out.println(gender);
                     tracker += 1;
                 }
-                else if (!gender.equals("male") || !gender.equals("female")) {
-                    System.out.println("Please enter in a valid gender.");
+
+                else if (gender.equals("male") || gender.equals("female")) {
+                    counter += 1;
+                    if (counter >= 1) {
+                        System.out.println("Please enter in a valid gender.");
+                    }
+
                 }
             }
+
 
         }
 
