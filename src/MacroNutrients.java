ccbbe9a2c0d3b2cf0b726169e21414048baca983
Josh Adebayo <Josh.Adebayo@InfiniteCampus.com>
2023-06-20 09:19:16 -0500

    Changed the structure of while loop to allow for multiple user inputs.

--- src/MacroNutrients.java
+++ src/MacroNutrients.java
@@ -3,20 +3,33 @@ import java.util.Scanner;
 public class MacroNutrients {
     public static void main(String[] args) {
         Scanner info = new Scanner(System.in);
+        int tracker = 1;
 
         System.out.println("How old are you?");
-        while(!info.equals("Stop")) {
-            int age = info.nextInt();
-            if ( age > 0) {
-                System.out.println(age);
+        while (tracker <= 3) {
+            if (tracker == 1) {
+                int age = info.nextInt();
+                if (age > 0) {
+                    System.out.println(age);
+                    tracker += 1;
+                    System.out.println(tracker);
+                }
+
+                else if (age <= 0) {
+                    System.out.println("Please enter in a valid age");
+                }
             }
-            else if (age <= 0) {
-                System.out.println("Please enter in a valid age");
-                continue;
-            }
-            String gender = info.nextLine();
-            if (gender == "male" || gender == "female") {
-                System.out.println(gender);
+
+
+            if (tracker == 2) {
+                System.out.println(tracker);
+                System.out.println("What is your gender?");
+                String gender = info.nextLine();
+                if (gender == "male" || gender == "female") {
+                    System.out.println(gender);
+                    tracker += 1;
+
+                }
             }
 
         }
