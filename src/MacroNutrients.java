a1902771c73a4eafbbb3e35d22fc942690221b04
Josh Adebayo <Josh.Adebayo@InfiniteCampus.com>
2023-06-20 08:21:55 -0500

    Added variables for age and gender.

--- src/MacroNutrients.java
+++ src/MacroNutrients.java
@@ -1,16 +1,25 @@
 import java.util.Scanner;
+
 public class MacroNutrients {
     public static void main(String[] args) {
         Scanner info = new Scanner(System.in);
 
-
-        while(!info.hasNextInt()) {
-            System.out.println("How old are you?");
-            String age = info.nextLine();
-            System.out.println(age);
-            // Test -m
-
+        System.out.println("How old are you?");
+        while(!info.equals("Stop")) {
+            int age = info.nextInt();
+            if ( age > 0) {
+                System.out.println(age);
+            }
+            else if (age <= 0) {
+                System.out.println("Please enter in a valid age");
+                continue;
+            }
+            String gender = info.nextLine();
+            if (gender == "male" || gender == "female") {
+                System.out.println(gender);
+            }
 
         }
+
     }
 }
